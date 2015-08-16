07bc0ef0a5737d42fc4b24bc364edb986e80ec76
Donald Adu-Poku <donald.adu@gmail.com>
2015-08-16 23:14:58 +0000

    added the barcode activity and its layout

--- app/src/main/java/dnldd/base/Activity.java
+++ app/src/main/java/dnldd/base/Activity.java
@@ -2,11 +2,12 @@ package dnldd.base;
 
 import android.os.Bundle;
 
-public class Activity extends dnldd.backpack.activity.DrawerActivity {
+public class Activity extends dnldd.backpack.activity.BarcodeActivity {
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+        setupBarcodeActivity(dnldd.backpack.R.layout.bp_scanner_layout);
         //setupDrawerActivity(R.layout.bp_drawer_activity_layout, R.menu.bp_drawer_view, R.drawable.ic_menu);
-        setupDrawerActivity(R.layout.bp_drawer_activity_layout, R.menu.bp_drawer_view, R.drawable.ic_menu, R.layout.nav_header);
+        //setupDrawerActivity(R.layout.bp_drawer_activity_layout, R.menu.bp_drawer_view, R.drawable.ic_menu, R.layout.nav_header);
     }
 }
